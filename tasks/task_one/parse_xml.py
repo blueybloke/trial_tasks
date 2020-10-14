831027a3ec2df80f24e226ff22c2f25915bdfd7a
Max Phillips <max@maxphillipsdev.com>
2020-10-14 22:42:59 +1100

    Finished task one.

--- tasks/task_one/parse_xml.py
+++ tasks/task_one/parse_xml.py
@@ -2,17 +2,47 @@
 import xml.etree.ElementTree as ET
 
 
-def parse_xml(xml_path: str):
+def parse_tokens(xml_path: str):
     """Parse a given XML file to get the metabolite data."""
     result: list = []
 
     # Load the XML file.
     xml = ET.parse(xml_path)
-    root = xml.getroot()
 
-    for metabolite in root:
+    for metabolite in xml.getroot():
+        # Generate normal_concentrations.
+        normal_concentrations = []
+        for con in metabolite.find('normal_concentrations'):
+            normal_concentrations.append({
+                'biospecimen':
+                    con.findtext('biospecimen').upper(),
+                'concentration_value':
+                    con.findtext('concentration_value').upper(),
+                'concentration_units':
+                    con.findtext('concentration_units').upper(),
+                'subject_age':
+                    con.findtext('subject_age').upper(),
+                'subject_sex':
+                    con.findtext('subject_sex').upper(),
+                'subject_condition':
+                    con.findtext('subject_condition').upper(),
+                'references': [
+                    ref.findtext('pubmed_id') for ref in con.find('references')
+                    ]
+            })
+
         result.append({
-            'name': metabolite.findtext('name')
+            'name':
+                metabolite.findtext('name').upper(),
+            'creation_date':
+                metabolite.findtext('creation_date').upper(),
+            'update_date':
+                metabolite.findtext('update_date').upper(),
+            'accession':
+                metabolite.findtext('accession').upper(),
+            'normal_concentrations':
+                normal_concentrations
+
+
         })
     return result
-
--- tasks/tests/test_tokens.py
+++ tasks/tests/test_tokens.py
@@ -1,46 +1,47 @@
 """Test the xml file is parsed correctly."""
 import unittest
 
-from ..task_one.parse_xml import parse_xml
+from ..task_one.parse_xml import parse_tokens
 
 
 class TestTokens(unittest.TestCase):
     """Test that the tokens are parsed correctly."""
 
-    def test_xml_parser(self):
+    def test_parse_tokens(self):
+        """Test method for parse_tokens"""
+        self.maxDiff = None
         """Test parsed XML file example.xml."""
         # Set the expected output to a list of metabolites.
         # Each dict represents a metabolite.
         expected_output: list = [
             {
-                'name': 'D-Glucose',
+                'name': 'D-GLUCOSE',
                 'creation_date': '2005-11-16 15:48:42 UTC',
                 'update_date': '2020-08-10 16:51:26 UTC',
                 'accession': 'HMDB0000122',
                 'normal_concentrations': [
                     {
-                        'biospecimen': 'blood',
+                        'biospecimen': 'BLOOD',
                         'concentration_value': '3100-5600',
-                        'concentration_units': 'um',
-                        'subject_age': None,
-                        'subject_sex': None,
-                        'subject_condition': 'normal',
-                        'references': [10832746]
+                        'concentration_units': 'UM',
+                        'subject_age': 'NOT SPECIFIED',
+                        'subject_sex': 'NOT SPECIFIED',
+                        'subject_condition': 'NORMAL',
+                        'references': ['10832746']
                     },
                     {
-                        'biospecimen': 'blood',
+                        'biospecimen': 'BLOOD',
                         'concentration_value': '3890-5550',
-                        'concentration_units': 'um',
-                        'subject_age': None,
-                        'subject_sex': None,
-                        'subject_condition': 'normal',
-                        'references': [29030856]
+                        'concentration_units': 'UM',
+                        'subject_age': 'NOT SPECIFIED',
+                        'subject_sex': 'NOT SPECIFIED',
+                        'subject_condition': 'NORMAL',
+                        'references': ['29030856']
                     }
                 ]
             }
         ]
-        actual_output = parse_xml('tasks/tests/example.xml')
-        print(actual_output)
+        actual_output = parse_tokens('tasks/tests/example.xml')
         self.assertListEqual(expected_output, actual_output)
 
 
