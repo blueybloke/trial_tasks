09966eb5a81cc45e32a708ffdb94efaa21f2e39e
Max Phillips <max@maxphillipsdev.com>
2020-10-14 18:37:34 +1100

    First commit, setup task one

---
+++ tests/test_tokens.py
@@ -0,0 +1,21 @@
+"""
+Test the xml file is parsed correctly.
+"""
+import unittest
+from .. import app
+
+
+class TestTokens(unittest.TestCase):
+    """
+    Test that the tokens are parsed correctly.
+    """
+
+    def test_xml_parser(self):
+        """
+        Test parsed XML file example.xml.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ trial_tasks/task_one/parse_xml.py
@@ -0,0 +1,4 @@
+"""Parses a given XML file to return a dictionary of tokens."""
+import xml.etree.ElementTree as ET
+
+
